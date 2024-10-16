be0fc3000acec6c78251afd3458a158484467383
Mathews Alves <mathewsalves1@outlook.com>
2024-10-16 12:52:01 +0100

    Criação de uma cena teste para os imigos. Criação de 1 inimigo e scripts para fazê-lo se mover. Ataulização dos scripts do player (ainda falta finalizar) Falta terminar o script de ataque do inimigo Adicinei biblioteca para novo sistema de input do jogador

---
+++ Underground Rebellion/Assets/Scripts/Agents/AgentAnimations.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AgentAnimations : MonoBehaviour
+{
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void WalkingAnimation(Vector2 movementInput)
+	{
+		LookDirection(movementInput);
+		animator.SetBool("Running", movementInput.magnitude > 0);
+	}
+
+	public void LookDirection(Vector2 direction)
+	{
+		var scale = transform.parent.localScale;
+		if (direction.x > 0)
+		{
+			scale.x = 1;
+		}
+		else if (direction.x < 0)
+		{
+			scale.x = -1;
+		}
+
+		transform.parent.localScale = scale;
+	}
+
+	//Caso queira colocar uma animação de morte que sabe da direção que vem a morte, para adicionar particulas ou knockback, é bom ter o sender no paramentro
+	public void DeathAnimation(GameObject sender)
+	{
+		animator.SetTrigger("death");
+	}
+
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
