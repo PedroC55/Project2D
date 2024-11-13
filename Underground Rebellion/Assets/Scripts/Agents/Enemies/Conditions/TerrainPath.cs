b5cc11b7af7ac76de479071c3019533e7f580dbf
Mathews Alves <mathewsalves1@outlook.com>
2024-11-13 02:59:32 +0000

    Some bug Fixes

--- Underground Rebellion/Assets/Scripts/Agents/Enemies/Conditions/TerrainPath.cs
+++ Underground Rebellion/Assets/Scripts/Agents/Enemies/Conditions/TerrainPath.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.IO.LowLevel.Unsafe;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class TerrainPath : MonoBehaviour
